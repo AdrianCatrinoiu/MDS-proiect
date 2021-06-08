d1308854bebbcea70087570d4302a9c41ba7fcce
marianeacsu <58784210+marianeacsu@users.noreply.github.com>
2021-06-08 20:16:43 +0300

    Update Diete.js

--- frontend/src/components/diete/Diete.js
+++ frontend/src/components/diete/Diete.js
@@ -1,1 +1,30 @@
+import React from "react";
+import Dieta from "../dieta/Dieta";
+import "../diete/Diete.css";
+
+export default class Diete extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const listDiete = this.props.diete.map((dieta) => (
+      // Correct! Key should be specified inside the array.
+      <Dieta
+        nume={dieta.type}
+        value={dieta.ammount}
+        ingredienteList={dieta.ingredients}
+        pregatire={dieta.preparation}
+        poza={dieta.picture}
+      />
+    ));
+    return <ul>{listDiete}</ul>;
+  }
+}
+
+// const numbers = [1, 2, 3, 4, 5];
+// ReactDOM.render(
+//   <Diete numbers={numbers} />,
+//   document.getElementById("root")
+// );
 
